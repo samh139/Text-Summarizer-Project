9719d37a9b834bd22d0f8139e994c35a4e27de58
Sameer H <sameer.herkal139@gmail.com>
2024-05-08 21:14:46 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from Text_Summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Text_Summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from Text_Summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from Text_Summarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from Text_Summarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 
 from Text_Summarizer.logging import logger
 
@@ -19,6 +23,41 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
